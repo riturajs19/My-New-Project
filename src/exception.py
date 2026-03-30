1f8cecd37994b7432e4c2fb207945f9dde324d12
Rituraj Singh <sriturajsingh745@gmail.com>
2026-03-30 22:54:34 +0530

    Second Commit

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,12 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb = error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     line_number = exc_tb.tb_lineno
     error_message = f"Error occurred in script: {file_name} at line number: {line_number} with error message: {str(error)}"
-    file_name, exc_tb.tb_lineno, error_message = error_message_detail(error, sys)
+  
     return error_message
 
 class CustomException(Exception):
